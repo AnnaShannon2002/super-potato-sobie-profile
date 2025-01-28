3a184dc221260c71132e910129daa909d320713d
AnnaShannon2002 <ashannon2@una.edu>
2025-01-28 13:39:50 -0600

    Updated 28

--- app.js
+++ app.js
@@ -15,25 +15,30 @@ app.get('/', function (req, res) {
 })
 
 app.post('/saveMyName', (req,res)=>{
-  console.log('did we git our end point?');
+  console.log('did we hit our end point?');
 
   console.log(req.body);
+
 
   // res.redirect('/ejs')
 
   res.render('words',
-  {theData : req.body});
+  {pageTitle: req.body.myName});
+
 
   // res.render('words',
   // {theData : req.body});
+
+
 })
 
 app.get('/saveMyNameGet', (req,res)=>{
-  console.log('did we git our end point?');
+  console.log('did we hit our end point?');
 
   console.log(req.query);
 
   res.redirect('/ejs')
+
 })
 
 app.get('/ejs', function (req, res) {
@@ -44,9 +49,9 @@ app.get('/ejs', function (req, res) {
 
 
 app.get('/nodemon', function (req, res) {
-  res.send('look ma, no kill node process then restart node then refresh browser...cool?')
-})
+  res.send('look ma, no kill node process then restart node then refresh browser...cool?');
 
+})
 
 // endpoint, middleware(s)
 app.get('/helloRender', function (req, res) {
@@ -54,9 +59,11 @@ app.get('/helloRender', function (req, res) {
 })
 
 
+
+
 app.listen(
   port, 
   ()=> console.log(
-    `server is running on  ...${port}`
+    `server is running on ... localhost:${port}`
   )
 );
