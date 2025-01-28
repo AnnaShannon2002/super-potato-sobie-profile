f37d24bb5a32fca186805868ee348b36b31cbc90
Shannon <ashannon2@una.edu>
2025-01-28 16:41:19 -0600

    yippee!!

--- app.js
+++ app.js
@@ -1,14 +1,47 @@
-require('dotenv').config()
 const express = require('express')
+require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser')
+// copied from mongodb
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.MONGO_URI;
+
+//console.log(uri);
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({entended: true}))
 app.use(express.static(__dirname + '/public'))
 
-// change my code
+
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+// This just says that we have connected to the database - Dr. Cumbie
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+// begin all my middlewares
 
 app.get('/', function (req, res) {
   res.sendFile('index.html')
@@ -35,9 +68,15 @@ app.post('/saveMyName', (req,res)=>{
 app.get('/saveMyNameGet', (req,res)=>{
   console.log('did we hit our end point?');
 
-  console.log(req.query);
+  console.log('req.query: ', req.query);
 
-  res.redirect('/ejs')
+  // console.log('req.params: ', req.params);
+
+  let reqName = req.query.myNameGet;
+  // res.redirect('/ejs')
+
+  res.render('words',
+  {pageTitle: reqName});
 
 })
 
@@ -48,6 +87,7 @@ app.get('/ejs', function (req, res) {
 })
 
 
+// Just messing around on this one.
 app.get('/nodemon', function (req, res) {
   res.send('look ma, no kill node process then restart node then refresh browser...cool?');
 
